e805a4cede30fd2fb8409eb35abc84b25005de34
panliang <1163816051@qq.com>
2021-08-13 10:27:04 +0800

    aliyun

--- config/aliyun.php
+++ config/aliyun.php
@@ -5,9 +5,9 @@ return [
     'access_secret' => env('ALIYUM_ACCESS_SECRET'),
     'oss' => [
         // 节点域名 例如: oss-cn-hangzhou.aliyuncs.com
-        'endpoint' => env('ALIYUN_OSS_ENDPOINT', ''),
+        'endpoint' => env('ALIYUN_OSS_ENDPOINT'),
         // 库名 例如: test
-        'bucket' => env('ALIYUN_OSS_BUCKET', ''),
+        'bucket' => env('ALIYUN_OSS_BUCKET'),
         // 自定义域名(选填), 例如: oss.example.com
         'domain' => env('ALIYUN_OSS_DOMAIN', ''),
         // 访问资源协议, 例如: http://
